4e1e44c01aace2d95adf7076502891a5d7b858ee
Krishna Avtar <krishnaavtar955@gmail.com>
2025-12-21 17:35:48 +0530

    increes media upload size

--- server/utils/fileUpload.js
+++ server/utils/fileUpload.js
@@ -71,7 +71,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
     storage: storage,
     limits: { 
-        fileSize: 5 * 1024 * 1024, // 5MB limit
+        fileSize: 20 * 1024 * 1024, // 5MB limit
         files: 1
     },
     fileFilter: fileFilter
